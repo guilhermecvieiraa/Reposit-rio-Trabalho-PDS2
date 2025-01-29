0b0b05d107b7f549b9e8e76a1108e4d131930e04
joão augusto <joaoaug10@gmail.com>
2025-01-29 01:50:56 +0000

    minhaparte

--- empresa.cpp
+++ empresa.cpp
@@ -3,6 +3,29 @@
 #include <string>
 #include <vector>
 
+
+class Endereco{
+  public:
+  std::string rua;
+  std::string bairro;
+  std::string cep;
+  int numero;
+  std::string complemento;
+ 
+ void salvarEndereco(const int){
+  std::ofstream arquivo("produtos.txt", std::ios::app);
+    if (arquivo.is_open()) {
+      arquivo << "Rua: " << rua << "\n";
+      arquivo << "Bairro: " << bairro << "\n";
+      arquivo << "CEP: " << cep << "\n";
+      arquivo << "Numero: " << numero << "\n";
+      arquivo << "Complemento: " << complemento << "\n";
+      arquivo << "-----------------------------\n";
+      arquivo.close();
+    }
+ }
+};
+
 class Produto {
 public:
   std::string nome;
@@ -24,26 +47,57 @@ public:
   }
 };
 
-class Departamento {
-  // Classe vazia por enquanto
+class Departamento{
+    public:
+        std::string nome;
+        std::string gestor;
+        std::vector<Funcionario> funcionarios;
+        std::string trabalhos;
+
+        void salvarDepartamento(){
+            std::ofstream arquivo("departamento.txt", std::ios::app);
+            if (arquivo.is_open()) {
+      arquivo << "Nome: " << nome<< "\n";
+      arquivo << "Gestor: " << gestor << "\n";
+      arquivo << "Funcionarios: ";
+      for (const auto& funcionario : funcionarios) {
+                arquivo << " - " << funcionario.nome << "\n";
+            }
+      arquivo << "Trabalhos: " << trabalhos << "\n";
+      arquivo << "-----------------------------\n";
+      arquivo.close();
+      arquivo << "-----------------------------\n";
+      arquivo.close();
+    }
+        }
 };
 
 class Refeitorio {
   // Classe vazia por enquanto
 };
 
+class Funcionario {
+  // Classe vazia por enquanto
+  std::string nome;
+};
+
 class Empresa {
 public:
   std::string nome;
-  std::string endereco;
+  Endereco endereco;
   std::string cnpj;
+  std::vector<Departamento> departamentos;
 
   void salvarEmpresa() {
     std::ofstream arquivo("empresa.txt", std::ios::app);
     if (arquivo.is_open()) {
       arquivo << "Nome: " << nome << "\n";
-      arquivo << "Endereço: " << endereco << "\n";
+      arquivo << "Endereço: " << endereco.rua << ", n°" << endereco.numero << " - " << endereco.bairro << " - " << endereco.cep;
       arquivo << "CNPJ: " << cnpj << "\n";
+      arquivo << "Departamentos:";
+      for (const auto& departamento: departamentos){;
+                arquivo << " - " << departamento.nome << "\n";
+            }
       arquivo << "-----------------------------\n";
       arquivo.close();
     }
@@ -53,15 +107,31 @@ public:
 int main() {
   Empresa empresa;
   Produto produto;
+  Departamento departamento;
+  Endereco endereco;
 
   std::cout << "Digite o nome da empresa: ";
   std::getline(std::cin, empresa.nome);
 
-  std::cout << "Digite o endereço da empresa: ";
-  std::getline(std::cin, empresa.endereco);
-
   std::cout << "Digite o CNPJ da empresa: ";
   std::getline(std::cin, empresa.cnpj);
+
+  std::cout << "Digite a rua da empresa: ";
+  std::getline(std::cin, endereco.rua);
+
+  std::cout << "Digite o bairro da empresa: ";
+  std::getline(std::cin, endereco.bairro);
+
+  std::cout << "Digite o CEP da empresa: ";
+  std::getline(std::cin, endereco.cep);
+
+  std::cout << "Digite o número da empresa: ";
+  std::cin, endereco.numero;
+
+  std::cout << "Digite o complemento do endereço da empresa: ";
+  std::getline(std::cin, endereco.complemento);
+
+
 
   empresa.salvarEmpresa();
 
@@ -89,6 +159,22 @@ int main() {
     std::cin >> continuar;
     std::cin.ignore();
   }
+
+  char continuar = 's';
+  while (continuar == 's') {
+    std::cout << "Digite o nome do departamento: ";
+    std::getline(std::cin, departamento.nome);
+
+    std::cout << "Digite o nome do gestor do departamento: ";
+    std::getline(std::cin, departamento.gestor);
+
+    std::cout << "Digite os trabalhos do departamento: ";
+    std::getline(std::cin, departamento.trabalhos);
+
+    departamento.salvarDepartamento();
+  }
+
+
 
   std::cout << "Informações salvas com sucesso!!" << std::endl;
   return 0;
