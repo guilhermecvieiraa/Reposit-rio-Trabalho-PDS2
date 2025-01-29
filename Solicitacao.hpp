503d4fe1974e6cc435f30061bdfb5d6a55885497
dennerleon <164677515+dennerleon@users.noreply.github.com>
2025-01-29 02:58:18 -0300

    Update Solicitacao.hpp
    Mudei A solicitacao.hpp pois agora esta implementada com a classe motoristas que estava fazendo

--- Solicitacao.hpp
+++ Solicitacao.hpp
@@ -1,28 +1,32 @@
 #ifndef SOLICITACAO_HPP
 #define SOLICITACAO_HPP
+
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include "Motorista.hpp"
+#include <list>
 
 using namespace std;
 
 class Solicitacao {
 private:
     string veiculo;
-    string motorista;
+    Motorista motorista;
     string endereco;
     double valorFrete;
     string produto;
+    static list<Motorista> bancoMotoristas;
 
 public:
-    // Para verificar se entrou dados, se nao tiver entrado a construtor da ""
-    Solicitacao(string veiculo = "", string motorista = "", string endereco = "", double valorFrete = 0.0, string produto = "");
-
-    void solicitarDados();
-
+    Solicitacao(string veiculo = "", Motorista motorista = Motorista(), string endereco = "", double valorFrete = 0.0, string produto = "");
+// corrigido
+    void solicitarDadosEntrega();
     void exibirDetalhes() const;
-
     void salvarEmArquivo();
+
+    static void cadastrarMotorista(const Motorista& m);
+    static bool verificarMotorista(const string& nomeMotorista);
 };
 
-#endif 
+#endif
